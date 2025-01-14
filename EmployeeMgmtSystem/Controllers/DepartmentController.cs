1ab484f8e7ca83aa69e27ff76f08b7bd440c92b0
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2025-01-15 01:36:44 +0600

    Worked on Department Controller

---
+++ EmployeeMgmtSystem/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeMgmtSystem.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMgmtSystem.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DepartmentController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
